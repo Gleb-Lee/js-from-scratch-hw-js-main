82538e3a27404b333174c3f3fd21712085364f65
Gleb Lee <oglebli1204@gmail.com>
2025-08-20 17:47:33 +0300

    factorial

---
+++ lesson-01/01.js
@@ -0,0 +1,16 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let sum
+let i = 0;
+let result = 0
+
+while(i < 1){
+    result += i;
+    i++;
+}
+
+console.log(result)
+
+
